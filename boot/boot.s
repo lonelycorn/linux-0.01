692633726cc08c73efd96c24c8b53020cddbd103
smegmachan <smegma8@gmail.com>
2021-03-09 23:11:54 -0800

    Add comment to boot.s head.s
    Signed-off-by: smegmachan <smegma8@gmail.com>

--- boot/boot.s
+++ boot/boot.s
@@ -1,3 +1,10 @@
+//本主引导记录采用intel汇编格式,as86编译,ld86链接
+| Power on ---> FFFF:0000 (0xFFFFFFF0 in 32bit) (0xFFFF0 in 20bit) --->
+| jmp to (0xFFFFE05B) ---> POST ---> setup isr (13(disk), 10(display),
+| 16(keyboard), 19(find mbr)) ---> int 0x19h (load mbr(1st sec)to 07C0:0000)
+| ---> jmp to 0x7C00(32KB).
+
+
 |
 |	boot.s
 |
@@ -50,6 +57,7 @@ BOOTSEG = 0x07c0
 INITSEG = 0x9000
 SYSSEG  = 0x1000			| system loaded at 0x10000 (65536).
 ENDSEG	= SYSSEG + SYSSIZE
+//SYSSIZE 在编译时候由编译工具按照编译脚本生成, 见makefile
 
 entry start
 start:
@@ -57,39 +65,45 @@ start:
 	mov	ds,ax
 	mov	ax,#INITSEG
 	mov	es,ax
+        | ds=07c0, es=9000
 	mov	cx,#256
 	sub	si,si
 	sub	di,di
+        | si=0, di=0
 	rep
 	movw
+        | copy 1 word (16byte) ds:si -> es:di (07c0:0000 -> 9000:0000) (section:offset)
+        | until cx==0. 256*16bit from 0x7c00(32KB) to 0x90000 (576KB)
+        //bios只加载了mbr(=512Bytes)到7c00, 只要挪256个word即可
 	jmpi	go,INITSEG
+        | jmp to INITSEG:go (9000:offset of "go")
+        | therefore cs=INITSEG=0x9000
 go:	mov	ax,cs
 	mov	ds,ax
 	mov	es,ax
 	mov	ss,ax
+        | ss=es=ds=cs=0x9000
 	mov	sp,#0x400		| arbitrary value >>512
-
+        | stack pointer = 9000:0400
 	mov	ah,#0x03	| read cursor pos
 	xor	bh,bh
-	int	0x10
-	
+	int	0x10            | display isr
 	mov	cx,#24
 	mov	bx,#0x0007	| page 0, attribute 7 (normal)
 	mov	bp,#msg1
-	mov	ax,#0x1301	| write string, move cursor
-	int	0x10
-
+	mov	ax,#0x1301	| write string, move cursor, ah = 0x13 (show)
+	int	0x10            | display isr
 | ok, we've written the message, now
 | we want to load the system (at 0x10000)
 
 	mov	ax,#SYSSEG
-	mov	es,ax		| segment of 0x010000
+	mov	es,ax		| segment of 0x010000, es = 0x1000
 	call	read_it
 	call	kill_motor
 
 | if the read went well we get current cursor position ans save it for
 | posterity.
-
+//读当前光标位置--->cs:510，bios 0x10中断，功能号0x03
 	mov	ah,#0x03	| read cursor pos
 	xor	bh,bh
 	int	0x10		| save it in known place, con_init fetches
@@ -102,8 +116,14 @@ go:	mov	ax,cs
 | first we move the system to it's rightful place
 
 	mov	ax,#0x0000
-	cld			| 'direction'=0, movs moves forward
+	cld			| 'direction'=0, movs moves forward, si+=2 di+=2 after movsw
+//将标志寄存器Flag的方向标志位DF清零.
+//在字串操作中使变址寄存器SI或DI的地址指针自动增加，字串处理由前往后。
 do_move:
+//移动整个system模块，从0x10000(64KB)到0x00000(0KB)处，当时假设整个系统
+//模块不会超过512KB(0x80000)，所以现在其末端地址不超过0x90000.
+//Boot:7c00(32KB)--->90000(576KB)，加载整个系统Floopy--->0x10000.
+//Whole system (0x10000---0x8FFFF) ---> (0x00000---0x7FFFF)
 	mov	es,ax		| destination segment
 	add	ax,#0x1000
 	cmp	ax,#0x9000
@@ -111,22 +131,25 @@ do_move:
 	mov	ds,ax		| source segment
 	sub	di,di
 	sub	si,si
-	mov 	cx,#0x8000
+	mov 	cx,#0x8000 //移动0x8000个字, 刚好64KB,一个段的大小
 	rep
-	movsw
+	movsw //源地址是DS:SI,目的地址是ES:DI
 	j	do_move
 
 | then we load the segment descriptors
 
 end_move:
-
+//代码现在依旧运行在0x9000段，cs=0x9000
+//这里将数据段寄存器ds也设置为与cs一样
+//目的是加载全局描述符和中断描述符, 保护模式要用到
 	mov	ax,cs		| right, forgot this at first. didn't work :-)
 	mov	ds,ax
 	lidt	idt_48		| load idt with 0,0
 	lgdt	gdt_48		| load gdt with whatever appropriate
 
 | that was painless, now we enable A20
-
+// 键盘相关的最重要的硬件是两个芯片。一个是 intel 8042 芯片，位于主板上，CPU 通过 IO 端口直接和这个芯片通信，获得按键的扫描码或者发送各种键盘命令。另一个是 intel 8048 芯片或者其兼容芯片，位于键盘中，这个芯片主要作用是从键盘的硬件中得到被按的键所产生的扫描码，与 i8042 通信，控制键盘本身。
+// 8042的P2端口位1是A20地址线控制位。A20 on之后就可以寻址1MB以上空间
 	call	empty_8042
 	mov	al,#0xD1		| command write
 	out	#0x64,al
@@ -142,16 +165,16 @@ end_move:
 | rectify it afterwards. Thus the bios puts interrupts at 0x08-0x0f,
 | which is used for the internal hardware interrupts as well. We just
 | have to reprogram the 8259's, and it isn't fun.
-
+//给8259中断控制器重新编程
 	mov	al,#0x11		| initialization sequence
 	out	#0x20,al		| send it to 8259A-1
 	.word	0x00eb,0x00eb		| jmp $+2, jmp $+2
 	out	#0xA0,al		| and to 8259A-2
 	.word	0x00eb,0x00eb
-	mov	al,#0x20		| start of hardware int's (0x20)
+	mov	al,#0x20		| start of hardware int's (0x20),主芯片起始中断号
 	out	#0x21,al
 	.word	0x00eb,0x00eb
-	mov	al,#0x28		| start of hardware int's 2 (0x28)
+	mov	al,#0x28		| start of hardware int's 2 (0x28)，从芯片起始中断号
 	out	#0xA1,al
 	.word	0x00eb,0x00eb
 	mov	al,#0x04		| 8259-1 is master
@@ -179,16 +202,31 @@ end_move:
 | things as simple as possible, we do no register set-up or anything,
 | we let the gnu-compiled 32-bit programs do that. We just jump to
 | absolute address 0x00000, in 32-bit protected mode.
-
 	mov	ax,#0x0001	| protected mode (PE) bit
+//将源操作数加载到机器状态字
 	lmsw	ax		| This is it!
+//只有源操作数的低 4 位（也就是 PE、MP、EM 及 TS 标志）会加载到 CR0。CR0 的 PG、CD、NW、AM、WP、NE 及 ET 标志不受影响。
+//PE=1, enter protection mode, 特权级CPL=0
 	jmpi	0,8		| jmp offset 0 of segment 8 (cs)
+//已经进入保护模式, cs=8,保护模式下的选择子selector=8, 2B=16bit长度
+//selector bit:
+// 0-1: RPL, requested privilege level
+//   2: TI, table indicator
+//3-15: index
+//0b 0000, 0000, 0000, 1000
+//   ^-----------------^:   index = 1, the 2nd descriptor
+//                      ^:  TI, select the GDTR
+//                       ^^:RPL = 0, 最高特权级
+//对应的全局描述符base address=0x0000, 偏移是jmpi第一个操作数
+//所以跳到0x0000绝对地址开始执行
 
 | This routine checks that the keyboard command queue is empty
 | No timeout is used - if this hangs there is something wrong with
 | the machine, and we probably couldn't proceed anyway.
 empty_8042:
-	.word	0x00eb,0x00eb
+//测试8042键盘控制器寄存器状态，等待输入buffer空
+	.word	0x00eb,0x00eb //JMP $+2 $:current IP
+//相当于两条NOP空操作, 但是耗时比NOP更高，NOP=3 ticks, 0x00eb=7-10 ticks
 	in	al,#0x64	| 8042 status port
 	test	al,#2		| is input buffer full?
 	jnz	empty_8042	| yes - loop
@@ -205,75 +243,90 @@ empty_8042:
 | (originally 18, for a 1.44Mb drive)
 |
 sread:	.word 1			| sectors read of current track
+//注意，不是从0扇区开始。0扇区对应的是MBR主引导记录，也就是本boot.s程序
 head:	.word 0			| current head
 track:	.word 0			| current track
 read_it:
-	mov ax,es
-	test ax,#0x0fff
-die:	jne die			| es must be at 64kB boundary
-	xor bx,bx		| bx is starting address within segment
+//从软盘读取SYSSIZE(当时假定不超过0x80000,512KB)数据去ES:BX
+//由于是从扇区1开始读的，所以直接就是head.s开始的程序了
+	mov ax,es               // es is 0x1000 now
+	test ax,#0x0fff         // bit-and and set Z flag (set if result is 0)
+die:	jne die			// es must be at 64kB boundary (jmp if Z flag is set)
+                                // es:bx must be a product of an integer and 64KB
+	xor bx,bx		// bx is starting address within segment
 rp_read:
+//判读是否已经读入全部数据。通过比较当前段是否就是系统数据末端所处段。
 	mov ax,es
 	cmp ax,#ENDSEG		| have we loaded all yet?
 	jb ok1_read
 	ret
 ok1_read:
-	mov ax,#sectors
+//计算当前磁道需要读取的扇区数--->ax
+	mov ax,#sectors         // 1.44MB=18sector per track
 	sub ax,sread
-	mov cx,ax
-	shl cx,#9
-	add cx,bx
-	jnc ok2_read
-	je ok2_read
-	xor ax,ax
-	sub ax,bx
-	shr ax,#9
+	mov cx,ax               // cx=# of unread sectors
+	shl cx,#9               // cx=*512==bytes unread
+//当前磁道还未读取的字节数加上段内数据开始处偏移位置
+	add cx,bx               // cx=bytes read into this sector(es:bx) after this reading op
+//不超过64KB的段内限制
+	jnc ok2_read            // j if not carry, (cx<0x10000) 64kb
+	je ok2_read             // j if equal (cx == 0x10000) 64kb
+//超过则反算扇区数
+	xor ax,ax               // ax=0
+	sub ax,bx               // ax=64kb'compliment of bx
+	shr ax,#9               // ax>>9
 ok2_read:
 	call read_track
-	mov cx,ax
-	add ax,sread
-	cmp ax,#sectors
-	jne ok3_read
-	mov ax,#1
-	sub ax,head
-	jne ok4_read
-	inc track
+	mov cx,ax  //返回值-->cx=# of sector read
+	add ax,sread // ax=+sread, 加上当前磁道已读扇区数
+	cmp ax,#sectors //当前磁道所有扇区已读？
+	jne ok3_read //否，j ok3_read
+	mov ax,#1 //已读，ax=1
+	sub ax,head //ax=-head, ax此时是下面要读取的数据对应的磁头号
+                    //head==0, ax=1; head==1,ax=0
+	jne ok4_read //jmp not equal, if ax==0，当前磁道磁头0面已读，换到1面读
+	inc track // ax==1,双面已读，换下一磁道
 ok4_read:
-	mov head,ax
-	xor ax,ax
+	mov head,ax //保存要读的磁头号
+	xor ax,ax //ax=0
 ok3_read:
-	mov sread,ax
-	shl cx,#9
-	add bx,cx
-	jnc rp_read
-	mov ax,es
-	add ax,#0x1000
-	mov es,ax
-	xor bx,bx
+// 如果当前磁道还有未读扇区，先保存当前磁道已读扇区数量，调整数据存放的起始位置(bx)
+// 若小于64KB, j rp_read. 否，调整段,j rp_read
+	mov sread,ax //保存当前磁道已读扇区数量
+	shl cx,#9 //cx是上次已读扇区数, cx>>9 变成已读字节数
+	add bx,cx //调整当前段内数据起始位置
+	jnc rp_read //j if 没有超过段限长
+	mov ax,es // es--->ax
+	add ax,#0x1000 //指向下一个段, +64KB
+	mov es,ax // ax--->es
+	xor bx,bx // bx=0
 	jmp rp_read
 
 read_track:
+//读取当前磁道上指定开始扇区和需要读取的扇区数的数据到es:bx处 (0x1000:0000开始写入system模块)
 	push ax
 	push bx
 	push cx
 	push dx
-	mov dx,track
-	mov cx,sread
-	inc cx
-	mov ch,dl
-	mov dx,head
-	mov dh,dl
-	mov dl,#0
-	and dx,#0x0100
-	mov ah,#2
-	int 0x13
-	jc bad_rt
+
+	mov dx,track //当前磁道号->dx
+	mov cx,sread //已读扇区数->cx
+	inc cx       //要读取的扇区号
+	mov ch,dl    //ch=当前磁道号
+	mov dx,head  //dx=当前磁头号
+	mov dh,dl    //dh=当前磁头号
+	mov dl,#0    //dl=驱动器号,0=A:\>
+	and dx,#0x0100 //磁头号不大于1,软盘最多2个头
+	mov ah,#2     //中断功能号，2=读扇区
+	int 0x13      //bios磁盘中断
+	jc bad_rt     //carry位表示读到坏区
+
 	pop dx
 	pop cx
 	pop bx
 	pop ax
 	ret
-bad_rt:	mov ax,#0
+bad_rt:	mov ax,#0      //reset driver
 	mov dx,#0
 	int 0x13
 	pop dx
@@ -287,34 +340,48 @@ bad_rt:	mov ax,#0
  * that we enter the kernel in a known state, and
  * don't have to worry about it later.
  */
-kill_motor:
+kill_motor: //关马达
 	push dx
-	mov dx,#0x3f2
-	mov al,#0
-	outb
+	mov dx,#0x3f2   //io address ---> FDC
+			//bit7-4(A,B,C,D盘on/off), bit3-2(DMA,irq on/off), bit1-0(selector, which FD)
+	mov al,#0       //irq, dma, FDC ---> off
+	outb //IO op
 	pop dx
 	ret
 
 gdt:
+//全局描述符表
+//第一个描述符reserved.
 	.word	0,0,0,0		| dummy
-
+//代码段和数据段在此描述符表内都是同一段内存,开始于0x00000000绝对地址
+//text segment descriptor
 	.word	0x07FF		| 8Mb - limit=2047 (2048*4096=8Mb)
 	.word	0x0000		| base address=0
 	.word	0x9A00		| code read/exec
 	.word	0x00C0		| granularity=4096, 386
-
+//data segment descriptor
 	.word	0x07FF		| 8Mb - limit=2047 (2048*4096=8Mb)
 	.word	0x0000		| base address=0
 	.word	0x9200		| data read/write
 	.word	0x00C0		| granularity=4096, 386
 
 idt_48:
+//中断描述符表,空。被加载到IDT寄存器
 	.word	0			| idt limit=0
 	.word	0,0			| idt base=0L
+/*
+Interrupt descriptor table (IDT) is an x86 system table that holds descriptors for Interrupt Service Routines (ISRs) or simply interrupt handlers.
 
+In real mode, there is an IVT (interrupt vector table) which is located by the fixed address 0x0 and contains “interrupt handler pointers” in the form of CS and IP registers values. This is really inflexible and relies on segmented memory management, and since 80286, there is an IDT for protected mode.
+
+IDT is the table in memory, created and filled by OS that is pointed by idtr system register which is loaded with lidt instruction. You can use IDT only in protected mode. IDT entries contain gate descriptors - not only addresses of interrupts handlers (ISRs) in 32-bit form but also flags and protection levels. IDT entries are descriptors that describe interrupt gates, and so in this sense, it resembles GDT and its segment descriptors.
+*/
 gdt_48:
+//lgdt操作数是6字节，这里的数据将会加载进GDT寄存器
 	.word	0x800		| gdt limit=2048, 256 GDT entries
+//GDT长度=0x800, 2KB. 2KB/8B(per descriptor) = 256个描述符
 	.word	gdt,0x9		| gdt base = 0X9xxxx
+//描述符表地址=0x9<<16+(gdt)标签的偏移地址
 	
 msg1:
 	.byte 13,10
--- boot/head.s
+++ boot/head.s
@@ -1,3 +1,4 @@
+//采用ATT汇编格式,GNU的gas86编译，gld86链接
 /*
  *  head.s contains the 32-bit startup code.
  *
@@ -8,31 +9,46 @@
 .text
 .globl _idt,_gdt,_pg_dir
 _pg_dir:
+//这里最后存放的是页目录(开启分页后使用)
+
 startup_32:
-	movl $0x10,%eax
+	movl $0x10,%eax //mov long word 0x00000010 -> eax
 	mov %ax,%ds
 	mov %ax,%es
 	mov %ax,%fs
 	mov %ax,%gs
-	lss _stack_start,%esp
-	call setup_idt
-	call setup_gdt
+//ds=es=fs=gs=0x10h=0b 0001 0000 都是数据段选择子寄存器
+//selector bit:
+// 0-1: RPL, requested privilege level
+//   2: TI, table indicator
+//3-15: index
+//RPL=0,GDT,index=0b10=2, the 2nd descriptor in GDT
+// 15 - 3 |  2 | 1 - 0
+//  index | TI |  RPL
+	lss _stack_start,%esp   //defined in kernel/sched.c, 设置堆栈位置
+	call setup_idt 		//设置中断描述符
+	call setup_gdt 		//设置全局描述符
 	movl $0x10,%eax		# reload all the segment registers
 	mov %ax,%ds		# after changing gdt. CS was already
 	mov %ax,%es		# reloaded in 'setup_gdt'
 	mov %ax,%fs
 	mov %ax,%gs
-	lss _stack_start,%esp
-	xorl %eax,%eax
+//同上，设定数据段选择子寄存器
+	lss _stack_start,%esp   //defined in kernel/sched.c, 设置堆栈位置
+
+	xorl %eax,%eax          //eax=0
 1:	incl %eax		# check that A20 really IS enabled
-	movl %eax,0x000000
-	cmpl %eax,0x100000
-	je 1b
+	movl %eax,0x000000      //eax--->0x000000
+	cmpl %eax,0x100000      //eax==[0x100000](1MB处的内存)
+	je 1b                   // if equal j backward to 1:
+                                // so, if A20 not enabled, 会down机
+
+//用于检查协处理器是否存在。ET bit in CR0.
 	movl %cr0,%eax		# check math chip
 	andl $0x80000011,%eax	# Save PG,ET,PE
 	testl $0x10,%eax
 	jne 1f			# ET is set - 387 is present
-	orl $4,%eax		# else set emulate bit
+	orl $4,%eax		# else set emulate bit (EM bit in CR0)
 1:	movl %eax,%cr0
 	jmp after_page_tables
 
@@ -48,20 +64,43 @@ startup_32:
  *  written by the page tables.
  */
 setup_idt:
-	lea ignore_int,%edx
+	lea ignore_int,%edx     //ignore_int表示的地址--->edx
 	movl $0x00080000,%eax
 	movw %dx,%ax		/* selector = 0x0008 = cs */
 	movw $0x8E00,%dx	/* interrupt gate - dpl=0, present */
+//eax=0x0008,(ignore_int表示的地址低2字节)
+//edx=(ignore_int表示的地址高2字节), 0x8E00
+//interrupt descriptor (8Bytes=64bits):
+//63-48: isr 入口地址高2字节
+//15-0:  isr 入口地址低2字节
+//31-16: 段选择符
+//47:    P标志,该段是否存在
+//46-45: DPL
+//44:    描述符类型, 0=system, 1=data/code
+//43-40: "D110=1110" 系统描述符类型,110表示中断描述符
+//       d:(调用)门的大小,1=32bit,0=16bit,即isr入口地址是32bit or 16bit?
+//39-32: reserved, must be 0
+//
+//       63 - 48  |  47 | 46 45 | 44 43 42 41 40 | 39 - 32
+//  edx: isr high | P=1 | DPL=0 |  0  1  1  1  0 |    0
+//           31 - 16     |  15 - 0
+//  eax: selector=0x0008 | isr low
+// selector bits:
+//   15 - 3 |   2  | 1 - 0
+//  index=1 | TI=0 | RPL=0
+//  对应描述符是全局描述符表中的第二项,index=1,此描述符说了:base=0x0000
+//  就是说，isr代码在0x0000+ignore_int处
 
-	lea _idt,%edi
-	mov $256,%ecx
+	lea _idt,%edi          //_idt表示的地址--->edi, _idt是中断描述符表
+	mov $256,%ecxi         //循环256次，中断描述符表有256个中断描述符
 rp_sidt:
-	movl %eax,(%edi)
+	movl %eax,(%edi)       //每个中断描述符由eax和edx的内容组成
 	movl %edx,4(%edi)
-	addl $8,%edi
+	addl $8,%edi           //移动到新地址，填充下一个中断描述符
 	dec %ecx
 	jne rp_sidt
-	lidt idt_descr
+	lidt idt_descr         //idt_descr处内容加载到中断描述符表寄存器
+//源操作数指定 6 字节内存位置，它包含全局描述符表格 (GDT) 或中断描述符表格 (IDT) 的基址（线性地址）与限制（表格大小，以字节计）。
 	ret
 
 /*
@@ -75,33 +114,38 @@ rp_sidt:
  *  This routine will beoverwritten by the page tables.
  */
 setup_gdt:
-	lgdt gdt_descr
+	lgdt gdt_descr //加载全局描述符表到GDT寄存器
 	ret
 
 .org 0x1000
-pg0:
+pg0: //页表0
 
 .org 0x2000
-pg1:
+pg1: //页表1
 
 .org 0x3000
-pg2:		# This is not used yet, but if you
+pg2: //页表2	# This is not used yet, but if you
 		# want to expand past 8 Mb, you'll have
 		# to use it.
 
+//0x4000以上地址最终会被页表填充
 .org 0x4000
 after_page_tables:
 	pushl $0		# These are the parameters to main :-)
 	pushl $0
 	pushl $0
+//envp=0, argc=0, argv=0
 	pushl $L6		# return address for main, if it decides to.
 	pushl $_main
 	jmp setup_paging
+//注意，这里是JMP不是CALL，但是setup_paging返回的时候是ret
+//所以直接跳转到_main函数执行
 L6:
 	jmp L6			# main should never return here, but
 				# just in case, we know what happens.
 
 /* This is the default interrupt "handler" :-) */
+//哑中断isr
 .align 2
 ignore_int:
 	incb 0xb8000+160		# put something on the screen
@@ -133,31 +177,88 @@ ignore_int:
  * some kind of marker at them (search for "8Mb"), but I
  * won't guarantee that's all :-( )
  */
-.align 2
+//大于1MB的内存空间被用于主内存，由mm模块管理。内核中其他函数(normal functions above)
+//用低于1MB的内存。主内存区的页面用get_free_page()取得。
+
+//物理地址0x0开始，放1页页目录和2页页表(内核专用,映射到8MB的范围)。页表2，即第三页页表，
+//并没有使用。新开的进程，在主内存区申请页面存放页表。
+//32Bit系统使用2级页表
+.align 2 //4字节对齐
 setup_paging:
-	movl $1024*3,%ecx
-	xorl %eax,%eax
-	xorl %edi,%edi			/* pg_dir is at 0x000 */
-	cld;rep;stosl
+	movl $1024*3,%ecx //设置计数器，清空3页内容
+	xorl %eax,%eax   //eax=0
+	xorl %edi,%edi	 //edi=0        /* pg_dir is at 0x000 */
+	cld;rep;stosl //edi正方向增长，eax--->es:edi
+//内核只用了2个页表，只对页目录设置2项
+//每个页表项(entry)占4Bytes,一张页表占1页(4KB),所以可以放1024项
+//页表项定义:
+//  31 - 12  | 11 - 9 | 8 |  7  | 6 | 5 |  4  |  3  | 2 | 1 | 0
+//  页帧地址     AVL    G   PAT   D   A   PCD   PWT  U/S R/W  P
+//  P: page exist?
+//  R/W: 0=RO, 1=writable
+//  U/S: 0=super 1=user
+//  PWT: 1=write through 0=write back (cache system)
+//  PCD: 1=disable cache
+//  A: accessed recently 1=yes (TLB system)
+//  D: dirty
+//  PAT:On the x86 with Pentium III and higher, this bit is called the Page Attribute Table (PAT)
+//      while earlier architectures such as the Pentium II had this bit reserved.
+//      The PAT bit is used to indicate the size of the page the PTE is referencing.
+//      In a PGD entry, this same bit is instead called the Page Size Exception (PSE) bit
+//      so obviously these bits are meant to be used in conjunction.
+//      在普通的4KB分页机制中, 处理器建议将其置0.
+//      页表项中为0.
+//  G:用来指示该表项所指向的页是否为全局性质的. 如果页是全局的, 那么, 它将在高速缓存中一直保存(也就意味着地址转换速度会很快). 因为页高速缓存容量有限, 只能存放频繁使用的那些表项. 而且, 当因任务切换等原因改变CR3寄存器的内容时, 整个页高速缓存的内容都会被刷新.(TLB system)
+//  AVL位被处理器忽略, 软件可以使用.
+
+// Line Address ---> Physical Address
+// LA             31 - 22           |            21 - 12             |         11 - 0
+//        Index to Page Directory          Index to Page Table                 Offset
+
 	movl $pg0+7,_pg_dir		/* set present bit/user r/w */
 	movl $pg1+7,_pg_dir+4		/*  --------- " " --------- */
-	movl $pg1+4092,%edi
-	movl $0x7ff007,%eax		/*  8Mb - 4096 + 7 (r/w user,p) */
-	std
+//页目录如下:
+//addr      pg_dir
+//0x0000    ($pg0+7) 0x00001007        // 页帧地址=0xFFFFF000 & 0x00001007 = 0x00001000
+                                       // U/S=1, R/W=1, P=1
+//0x0004    ($pg1+7) 0x00002007        // 页帧地址=0xFFFFF000 & 0x00001007 = 0x00002000
+                                       // U/S=1, R/W=1, P=1
+//以下填充页表(pg0,pg1)的页表项
+	movl $pg1+4092,%edi            // 最后一页最后一项对应地址(0x00002ffc)--->edi
+	movl $0x007ff007,%eax	/*  8Mb - 4096 + 7 (r/w user,p) */
+                                //物理内存地址8M-4KB的起始位置(0x007ff000)
+	std                     //方向置位，edi往地址减少方向
 1:	stosl			/* fill pages backwards - more efficient :-) */
-	subl $0x1000,%eax
-	jge 1b
-	xorl %eax,%eax		/* pg_dir is at 0x0000 */
+                                //eax--->es:edi
+	subl $0x1000,%eax       //eax-=0x1000,每填好一项，物理地址减0x1000
+	jge 1b                  //当eax>=0x1000时,页表未填充完毕,jmp backward to 1:
+                                //当eax<0x1000时,(此时eax为0x00000007)表示填好,不跳转
+//填充后页表内容:
+//addr      pg0                 phy addr
+//0x1000    0x00000007   --->   0x00000000 - 0x00000fff
+//0x1004    0x00001007   --->   0x00001000 - 0x00001fff
+// ...
+//0x1FF8    0x003fe007   --->   0x003fe000 - 0x003fefff
+//0x1FFC    0x003ff007   --->   0x003ff000 - 0x003fffff
+//addr      pg1
+//0x2000    0x00400007   --->   0x00400000 - 0x00400fff
+//0x2004    0x00401007   --->   0x00401000 - 0x00401fff
+// ...
+//0x2FF8    0x004fe007   --->   0x004fe000 - 0x004fefff
+//0x2FFC    0x004ff007   --->   0x004ff000 - 0x004fffff
+
+	xorl %eax,%eax		/* pg_dir is at 0x0000 */ //eax=0
 	movl %eax,%cr3		/* cr3 - page directory start */
+                                //页目录地址(0x00000000)--->CR3
 	movl %cr0,%eax
-	orl $0x80000000,%eax
+	orl $0x80000000,%eax    //开启分页
 	movl %eax,%cr0		/* set paging (PG) bit */
 	ret			/* this also flushes prefetch-queue */
 
 .align 2
 .word 0
 idt_descr:
-	.word 256*8-1		# idt contains 256 entries
+	.word 256*8-1		# idt contains 256 entries //中断描述符表大小in bytes
 	.long _idt
 .align 2
 .word 0
@@ -169,7 +270,7 @@ gdt_descr:
 _idt:	.fill 256,8,0		# idt is uninitialized
 
 _gdt:	.quad 0x0000000000000000	/* NULL descriptor */
-	.quad 0x00c09a00000007ff	/* 8Mb */
-	.quad 0x00c09200000007ff	/* 8Mb */
+	.quad 0x00c09a00000007ff	/* 8Mb */ //与boot.s中设置的一样
+	.quad 0x00c09200000007ff	/* 8Mb */ //与boot.s中设置的一样
 	.quad 0x0000000000000000	/* TEMPORARY - don't use */
 	.fill 252,8,0			/* space for LDT's and TSS's etc */
